84da626c2d57ad770b7ee0fd77f78cca82616bad
nikusha55 <166060954+nikusha55@users.noreply.github.com>
2025-02-16 18:25:22 +0300

    Delete exam directory

--- exam/5problem.py
+++
@@ -1,8 +0,0 @@
-def anagrams(txt1, txt2):
-    return sorted(txt1)==sorted(txt2)
-        
-         #Test Cases:
-print(anagrams("listen", "silent") == True)
-print(anagrams("hello", "world") == False)
-print(anagrams("triangle", "integral") == True)
-      
